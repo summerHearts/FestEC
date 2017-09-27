0b415f370d82d3171ab1a1fb2640305a524e3f5e
姚亚杰 <yaoyj@oriental-finance.com>
2017-09-27 12:48:03 +0800

    add icon

--- example/src/main/java/com/example/kenvin/festec/ExampleApp.java
+++ example/src/main/java/com/example/kenvin/festec/ExampleApp.java
@@ -3,6 +3,7 @@ package com.example.kenvin.festec;
 import android.app.Application;
 
 import com.example.latte.app.Latte;
+import com.joanzapata.iconify.fonts.FontAwesomeModule;
 
 /**
  * Created by Kenvin on 2017/9/27.
@@ -13,7 +14,7 @@ public class ExampleApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Latte.init(this).withApiHost("http:127.0.0.1/").configure();
+        Latte.init(this).withApiHost("http:127.0.0.1/").withIcon(new FontAwesomeModule()).configure();
     }
 
 
--- latte-core/src/main/java/com/example/latte/app/Configurator.java
+++ latte-core/src/main/java/com/example/latte/app/Configurator.java
@@ -1,5 +1,9 @@
 package com.example.latte.app;
 
+import com.joanzapata.iconify.Iconify;
+import com.joanzapata.iconify.IconFontDescriptor;
+
+import java.util.ArrayList;
 import java.util.WeakHashMap;
 
 /**
@@ -9,6 +13,8 @@ import java.util.WeakHashMap;
 public class Configurator {
 
     private  static  final WeakHashMap<String,Object> LATTE_CONFIGS = new WeakHashMap<>();
+
+    private  static  final ArrayList<IconFontDescriptor> ICONS = new ArrayList<>();
 
     private  Configurator(){
 
@@ -21,6 +27,7 @@ public class Configurator {
     }
 
     public  final void configure(){
+        initIcons();
         LATTE_CONFIGS.put(ConfigType.CONFIG_READY.name(),true);
     }
 
@@ -42,6 +49,21 @@ public class Configurator {
         if (!isReady){
             throw new RuntimeException("Configuration is not ready ,call configure");
         }
+    }
+
+    private void  initIcons (){
+
+        if (ICONS.size()>0){
+            final Iconify.IconifyInitializer  initializers = Iconify.with(ICONS.get(0));
+            for (int i = 1;i< ICONS.size();i++){
+                initializers.with(ICONS.get(i));
+            }
+        }
+    }
+
+    public final  Configurator withIcon(IconFontDescriptor descriptor){
+        ICONS.add(descriptor);
+        return this;
     }
 
     @SuppressWarnings("unckecked")
